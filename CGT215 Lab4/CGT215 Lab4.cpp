a3105ca21020d5fbb095b3c7ae8faad3cb21e10a
atunsaku <atunsaku@X-KNOY306PC12>
2022-09-13 14:05:12 -0400

    Add project files.

---
+++ CGT215 Lab4/CGT215 Lab4.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int input1;
+int input2;
+int input3;
+int output;
+
+// Print out the menu of choices for the user to select from
+void printMenu() {
+	cout << "Please Select which operation to perform:" << endl;
+	cout << "\t1. Factorial" << endl;
+	cout << "\t2. Arithmetic Series" << endl;
+	cout << "\t3. Geometric Series" << endl;
+	cout << "\t4. Exit" << endl;
+	cout << "Your Selection: ";
+}
+void factorial() {
+	// I recommend writing your factorial code here
+	int temp = 1;
+
+	cout << "Factorial" << endl;
+
+	do 
+	{
+		cout << "Enter a positive integer" << endl;
+		cin >> input1;
+	} while (input1 < 0);
+
+	for (int i = 1; i <= input1; i++)
+	{
+		temp *= i;
+		cout << temp << endl;
+	}
+}
+void arithmetic() {
+	// I recommend writing your arithmetic series code here
+}
+void geometric() {
+	// I recommend writing your geometric series code here
+}
+int main() {
+	int choice;
+	char again;
+	do {
+		printMenu();
+		cin >> choice;
+		// Quit if user chooses to exit (or any invalid choice)
+		if (choice > 3 || choice < 1) {
+			return 0;
+		}
+		else if (choice == 1) {
+			factorial();
+		}
+		else if (choice == 2) {
+			arithmetic();
+		}
+		else if (choice == 3) {
+			geometric();
+		}
+		cout << "Go Again? [Y/N] ";
+		cin >> again;
+	} while (again == 'y' || again == 'Y');
+}
